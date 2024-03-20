27f3a6d64221e62c8e04fdfe432332a02821fe44
aaron-muti-420 <mutiaaron2@gmail.com>
2024-03-20 10:57:42 +0200

    list added chirps

--- app/Http/Controllers/ChirpController.php
+++ app/Http/Controllers/ChirpController.php
@@ -3,18 +3,22 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+//use Illuminate\Http\Response; 
 use Illuminate\View\View;
 
  
 class ChirpController extends Controller
 {
-        /**
+    /**
      * Display a listing of the resource. 
      */
+    //public function index(): Response
     public function index(): View
     {
+        //return response('Hello, World!');-Change to chirps
         return view('chirps', [
-            //
+            
         ]);
+
     }
 }
--- routes/web.php
+++ routes/web.php
@@ -14,16 +14,17 @@ use App\Http\Controllers\ChirpController;
 */
 
 Route::view('/', 'welcome');
+ 
 Route::get('chirps', [ChirpController::class, 'index'])
     ->middleware(['auth', 'verified'])
     ->name('chirps'); 
-    
+ 
 Route::view('dashboard', 'dashboard')
     ->middleware(['auth', 'verified'])
     ->name('dashboard');
-
+ 
 Route::view('profile', 'profile')
     ->middleware(['auth'])
     ->name('profile');
-
+ 
 require __DIR__.'/auth.php';
